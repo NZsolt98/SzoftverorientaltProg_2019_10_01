4039ad3cf186e50e66585df7b4a759eadb3e9977
NZsolt98 <43852977+NZsolt98@users.noreply.github.com>
2019-10-01 18:12:44 +0200

    Mai ora elso programja
    Termelo fogyaszto

---
+++ PrimTermeloFogyaszto/PrimTermeloFogyaszto/Classes/Szamok.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace PrimTermeloFogyaszto
+{
+    class Szamok
+    {
+        public static List<int> list = new List<int>();
+        public static int termoDB = 0;
+
+        public static void Insert(int szam)
+        {
+            lock (list)
+            {
+                list.Add(szam);
+            }
+        }
+
+        public static void TermeloCsokkent()
+        {
+            lock (typeof(Szamok))
+            {
+                termoDB--;
+
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Egy termelő leállt!");
+                Console.ResetColor();
+
+                if (termoDB == 0)
+                {
+                    lock (list)
+                    {
+                        Monitor.PulseAll(list);
+                    }
+                }
+            }
+        }
+    }
+}
